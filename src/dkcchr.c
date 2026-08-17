0b7c7ef5cbd6bcc226deba39294dfa153fe05e29
Kingizor <kingizor@gmail.com>
2026-08-17 20:01:23 +0100

    dkcchr: update copyright notice

--- src/dkcchr.c
+++ src/dkcchr.c
@@ -1,5 +1,5 @@
 /* SPDX-License-Identifier: MIT
- * Copyright (c) 2020-2022 Kingizor
+ * Copyright (c) 2020-2026 Kingizor
  * dkcomp library - DKC CHR compressor and decompressor */
 
 #include "dk_internal.h"
